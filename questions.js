3f5fd43052f7d160a3908994099429aa8d092437
chelsie mahoney <chelsiesindar@gmail.com>
2022-01-14 17:17:57 -0700

    fix questions

--- questions.js
+++ questions.js
@@ -2,27 +2,27 @@
 var questions = [
     {
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        choices: ["quotes", "parentheses”, "curly brackets", "square brackets"],
         answer: "parentheses"
     },
   {
         title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
+        choices: [“alerts”, “strings”, “numbers”, “booleans”],
         answer: "alerts"
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        choices: ["other arrays", "numbers and strings", "booleans", "all of the above"],
         answer: "all of the above"
     },
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        choices: [“for loops”, "terminal / bash", “Javascript”, "console log"],
         answer: "console log"
     },
 
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: [“commas”, "curly brackets", "quotes", "parenthesis"],
+        choices: [“quotes”, "curly brackets", “commas”, "parenthesis"],
         answer: "quotes"
     },
     {
